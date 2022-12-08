8a57dadabedbf37c8ad5d4be91ad1b8ed023dad8
allamorim <allamorim98@gmail.com>
2022-12-08 18:33:59 +0000

    Day 7 part 2 completed

--- Day-7-Advent-of-Code-2022/src/com/AndreAmorim/Main.java
+++ Day-7-Advent-of-Code-2022/src/com/AndreAmorim/Main.java
@@ -4,12 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 
 public class Main {
@@ -55,32 +52,30 @@ public class Main {
             while ((st = br.readLine()) != null) {
 
 
-                    if (st.contains("$ cd")) {
-                        if (st.contains("..")) {
-                            actual_path = actual_path.replaceAll("/[A-z 1-9.]+$","");
-                        } else {
-                            st = st.replaceAll("\\$ cd ", "");
-                            actual_path += "/" + st;
-                            boolean exists = false;
-                            for (Directory d : directories) {
-                                if (d.getDir().equals(st)) {
-                                    exists = true;
-                                    break;
-                                }
-                            }
-                            if (exists == false) {
-                                directories.add(new Directory(actual_path));
+                if (st.contains("$ cd")) {
+                    if (st.contains("..")) {
+                        actual_path = actual_path.replaceAll("/[A-z 1-9.]+$", "");
+                    } else {
+                        st = st.replaceAll("\\$ cd ", "");
+                        actual_path += "/" + st;
+                        boolean exists = false;
+                        for (Directory d : directories) {
+                            if (d.getDir().equals(st)) {
+                                exists = true;
+                                break;
                             }
                         }
+                        if (exists == false) {
+                            directories.add(new Directory(actual_path));
+                        }
                     }
+                }
                 Pattern pattern = Pattern.compile("[0-9]+", Pattern.CASE_INSENSITIVE);
                 Matcher matcher = pattern.matcher(st);
                 boolean matchFound = matcher.find();
-                if(matchFound) {
-                    for (Directory d: directories)
-                    {
-                        if(d.getDir().equals(actual_path))
-                        {
+                if (matchFound) {
+                    for (Directory d : directories) {
+                        if (d.getDir().equals(actual_path)) {
 
                             d.addsize(Integer.parseInt(matcher.group()));
 
@@ -89,28 +84,22 @@ public class Main {
                 }
 
             }
-            for (Directory d : directories)
-            {
+            for (Directory d : directories) {
 
-                    String parentspath = d.getDir();
-                    while (!parentspath.equals("//"))
-                    {
-                        parentspath = parentspath.replaceAll("/[A-z 1-9.]+$","");
-                        for (Directory parents: directories)
-                        {
-                            if(parents.getDir().equals(parentspath))
-                            {
-                                parents.addsize(d.getSize());
-                            }
+                String parentspath = d.getDir();
+                while (!parentspath.equals("//")) {
+                    parentspath = parentspath.replaceAll("/[A-z 1-9.]+$", "");
+                    for (Directory parents : directories) {
+                        if (parents.getDir().equals(parentspath)) {
+                            parents.addsize(d.getSize());
                         }
                     }
+                }
 
             }
-            for (Directory d : directories)
-            {
-                if (d.getSize()<100000)
-                {
-                    number+=d.getSize();
+            for (Directory d : directories) {
+                if (d.getSize() < 100000) {
+                    number += d.getSize();
                 }
             }
 
@@ -129,39 +118,88 @@ public class Main {
 
             String st;
             int number = 0;
-            List<Character> chars_passed = new ArrayList<>();
+            List<Directory> directories = new ArrayList<>();
+            String actual_path = "";
             while ((st = br.readLine()) != null) {
 
-                for (int i = 0; i< st.length();i++)
-                {
-                    number++;
-                    if (chars_passed.contains(st.charAt(i)))
-                    {
-                        for (int y = chars_passed.indexOf(st.charAt(i)); y>= 0; y--)
-                            chars_passed.remove(y);
 
-                        chars_passed.add(st.charAt(i));
-
-                    }
-                    else
-                    {
-
-                        chars_passed.add(st.charAt(i));
-                        if (chars_passed.size()==14)
-                        {
-                            return number;
+                if (st.contains("$ cd")) {
+                    if (st.contains("..")) {
+                        actual_path = actual_path.replaceAll("/[A-z 1-9.]+$", "");
+                    } else {
+                        st = st.replaceAll("\\$ cd ", "");
+                        actual_path += "/" + st;
+                        boolean exists = false;
+                        for (Directory d : directories) {
+                            if (d.getDir().equals(st)) {
+                                exists = true;
+                                break;
+                            }
+                        }
+                        if (exists == false) {
+                            directories.add(new Directory(actual_path));
                         }
                     }
-
                 }
+                Pattern pattern = Pattern.compile("[0-9]+", Pattern.CASE_INSENSITIVE);
+                Matcher matcher = pattern.matcher(st);
+                boolean matchFound = matcher.find();
+                if (matchFound) {
+                    for (Directory d : directories) {
+                        if (d.getDir().equals(actual_path)) {
+
+                            d.addsize(Integer.parseInt(matcher.group()));
+
+                        }
+                    }
+                }
+
+            }
+            for (Directory d : directories) {
+
+                String parentspath = d.getDir();
+                while (!parentspath.equals("//")) {
+                    parentspath = parentspath.replaceAll("/[A-z 1-9.]+$", "");
+                    for (Directory parents : directories) {
+                        if (parents.getDir().equals(parentspath)) {
+                            parents.addsize(d.getSize());
+                        }
+                    }
+                }
+
             }
 
-            return -2;
+            List<Directory> dirs_with_space = new ArrayList<>();
+            int total =0;
+            for (Directory d : directories) {
+               if (d.getDir().equals("//"))
+               {
+                   total=d.getSize();
+               }
+
+            }
+            int total_needed = 70000000-total;
+            total_needed = 30000000-total_needed;
+            for (Directory d : directories) {
+                if (d.getSize() > total_needed) {
+                    dirs_with_space.add(d);
+                }
+
+            }
+            int smalldir = 0;
+            for (Directory d : dirs_with_space)
+                if (d.getSize()<smalldir || smalldir == 0)
+                {
+                    smalldir=d.getSize();
+                }
+
+           return smalldir;
 
         } catch (Exception e) {
             System.out.println("Error in the counting!");
             return -1;
 
         }
+
     }
 }
